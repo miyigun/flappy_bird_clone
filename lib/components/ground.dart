2441c9f9d23dd52d1756fba48dc1461738be691e
Murat Iyigun <miyigun@hotmail.com>
2024-01-21 23:41:06 +0300

    Commit 8

--- lib/components/ground.dart
+++ lib/components/ground.dart
@@ -1,3 +1,4 @@
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/parallax.dart';
@@ -5,15 +6,23 @@ import 'package:flappy_bird_clone/game/assets.dart';
 import 'package:flappy_bird_clone/game/config.dart';
 import 'package:flappy_bird_clone/game/flappy_bird_game.dart';
 
-class Ground extends ParallaxComponent<FlappyBirdGame> {
+class Ground extends ParallaxComponent<FlappyBirdGame> with HasGameRef<FlappyBirdGame>{
   Ground();
 
   @override
   Future<void> onLoad() async {
     final ground = await Flame.images.load(Assets.ground);
     parallax =
-        Parallax([ParallaxLayer(ParallaxImage(ground, fill: LayerFill.none))]);
+        Parallax([ParallaxLayer(ParallaxImage(ground, fill: LayerFill.none))],
+        );
+
+    add(RectangleHitbox(
+      position: Vector2(0, gameRef.size.y-Config.groundHeight),
+      size: Vector2(gameRef.size.x, Config.groundHeight),
+    ));
   }
+
+
 
   @override
   void update(double dt) {
